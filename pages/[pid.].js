0460bdd4c4659c1fc8a41fc44591569dc3d1e829
sajjan-17 <sajjanmandarav@gmail.com>
2024-02-23 13:32:31 +0530

    pre rendering the pages

---
+++ pages/[pid.].js
@@ -0,0 +1,26 @@
+import { Fragment } from "react";
+import fs from 'fs/promises'
+import path from 'path';
+
+function ProductDetailPage(){
+    
+    return(
+        <Fragment>
+            <h1>TITLE</h1>
+            <p> DESCRIPTION</p>
+        </Fragment>
+    )
+
+}
+export async function getStaticProps(context){
+  const {params} = context;
+
+  const productId= params.pid;
+  const filePath= path.join(process.cwd(),'data', 'dummy-backend.json');
+    const jsonData = await fs.readFile(filePath)
+    const data =JSON.parse(jsonData);
+
+}
+
+
+export default ProductDetailPage;
